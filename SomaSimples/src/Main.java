8d2add339529ce49a074b23d48c3140473a274f3
Izabela Alcantara <felixizabela16@gmai.com>
2021-04-27 20:39:10 -0300

    exercicio soma simples.

---
+++ SomaSimples/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		int A;
+		int B;
+		int SOMA;
+		
+		A = leitor.nextInt();
+		B = leitor.nextInt();
+		
+		SOMA = A + B;
+		
+		System.out.println("SOMA = " + SOMA);
+		
+
+	}
+
+}
